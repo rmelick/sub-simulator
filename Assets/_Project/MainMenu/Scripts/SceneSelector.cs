dacb668de3c98cc44fc20385172212016c0f02d2
Russell Melick <russell.melick@kiwigrid.com>
2016-08-18 21:34:12 +0200

    Add scene loading button

---
+++ Assets/_Project/MainMenu/Scripts/SceneSelector.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneSelector : MonoBehaviour {
+	private string _currentSceneToLoad;
+	
+
+	void Start() {
+		_currentSceneToLoad = "Pool1Scene";
+	}	
+
+	public void LoadScene() {
+		SceneManager.LoadScene(_currentSceneToLoad);
+	}
+}
