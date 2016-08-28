ed3498690cc835902b248264c75dc19b980406ac
Russell Melick <russell.melick@kiwigrid.com>
2016-08-28 16:56:56 +0200

    Modify control so they are less crazy

--- Assets/_Project/Robots/OpenRov/OpenRovControl.cs
+++ Assets/_Project/Robots/OpenRov/OpenRovControl.cs
@@ -8,12 +8,43 @@ public class OpenRovControl : MonoBehaviour {
 
 	// TODO make thrust power private?
 	public float _thrustPower;
+
+	// TODO remove this reference and switch to location based thrust
+	public Rigidbody _rovRigidBody;
 	// Use this for initialization
 	void Start () {
+		_rovRigidBody = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		float horizontal = Input.GetAxis(Axes.HORIZONTAL);
+		applyTorque(horizontal * Vector3.up);
+
+		float forward = Input.GetAxis(Axes.FORWARD);
+		Vector3 forwardThrustVector = _thrustPower * forward * Vector3.forward;
+		applyForce(forwardThrustVector);
+		
+		float depth = Input.GetAxis(Axes.DEPTH);
+		Vector3 depthThrustVector = _thrustPower * depth * Vector3.up * 2;
+		applyForce(depthThrustVector);
+		
+	}
+
+	private void applyTorque(Vector3 torque) {
+		_rovRigidBody.AddRelativeTorque(torque);
+		
+	}
+	private void applyForce(Vector3 force) {
+		Vector3 globalPosition = _rovRigidBody.transform.TransformPoint(_rovRigidBody.centerOfMass);
+		Vector3 globalDirection = _rovRigidBody.transform.TransformDirection(force);
+		Debug.DrawRay(globalPosition, globalDirection, Color.red);
+		_rovRigidBody.AddRelativeForce(globalDirection);
+	}
+
+	// we can't use this yet because the OpenRov model does not have a good center of mass
+	private void locationSensitiveUpdate() {
 		float horizontal = Input.GetAxis(Axes.HORIZONTAL);
 		// note that the rear thrusters are mounted facing backwards
 		// so their power needs to be reversed (that's why starboard
